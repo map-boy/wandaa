ac9ffee31e2477fe1564c9b1ee1bef6a43b4b4dc
map-boy <map-boy@users.noreply.github.com>
2026-09-13 03:38:16 +0200

    Fix build.ps1: don't prepend .\ to already-absolute exe path

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-﻿param([string]$Source = "examples\mbere.waa")
+param([string]$Source = "examples\mbere.waa")
 g++ -std=c++17 -O2 -o wandaac.exe src\lexer.cpp src\parser.cpp src\codegen.cpp src\main.cpp
 if ($LASTEXITCODE -ne 0) { exit 1 }
 .\wandaac.exe $Source
@@ -7,4 +7,4 @@ $exe = [System.IO.Path]::ChangeExtension($Source, ".exe")
 gcc -nostdlib -nostartfiles "-Wl,--entry=_start" "$Source.s" -o $exe -lkernel32
 if ($LASTEXITCODE -ne 0) { exit 1 }
 Write-Host "Byubatswe: $exe"
-& ".\$exe"
+& "$exe"
